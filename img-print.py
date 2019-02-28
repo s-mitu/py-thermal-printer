af17fe9b18a59f49a4ff999fffe45dd3e6e1bbc9
Mitsuhiro Suzuki <s_mitu@os.rim.or.jp>
2019-02-28 14:32:36 +0900

    Image print script

---
+++ img-print.py
@@ -0,0 +1,26 @@
+#!/usr/local/bin/python2
+# coding:utf-8
+
+import printer
+from PIL import Image
+import sys
+port = "/dev/cuaU0"
+brate = 115200
+
+if len(sys.argv) > 1:
+    imagefile = sys.argv[1]
+    if len(sys.argv) > 2:
+        port = sys.argv[2]
+    if len(sys.argv) > 3:
+        brate = int(sys.argv[3])
+else:
+    print("File name ")
+    exit(1)
+
+p = printer.ThermalPrinter(serialport=port, baudrate=brate)
+i = Image.open(imagefile)
+data = list(i.getdata())
+w, h = i.size
+p.print_bitmap(data, w, h)
+
+p.linefeed(3)
